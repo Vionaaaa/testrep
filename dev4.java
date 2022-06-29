fa03691f8cbc1b8adac8bf84ea7c597405a301a1
jingzhix <jingzx30669@163.com>
2022-06-29 13:19:06 +0800

    dev分支模拟成bug分支

---
+++ dev4.java
@@ -0,0 +1,11 @@
+/**
+ * desc
+ *
+ * @author jingzhixing
+ * @date 2022/6/29 周三
+ */
+public class dev4 {
+    public static void main(String[] args) {
+        System.out.println("这个假设是一个bug分支");
+    }
+}
