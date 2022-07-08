693966b9a240d6e502ce283d5716bc9919af0366
jingzhix <jingzx30669@163.com>
2022-07-08 11:17:31 +0800

    main分支新增class

---
+++ MainDevlop.java
@@ -0,0 +1,11 @@
+/**
+ * desc
+ *
+ * @author jingzhixing
+ * @date 2022/6/29 周三
+ */
+public class MainDevlop {
+    public static void main(String[] args) {
+        System.out.println("main分支上正在开发需求，但是还没做完，现在来了一个bug要改");
+    }
+}
